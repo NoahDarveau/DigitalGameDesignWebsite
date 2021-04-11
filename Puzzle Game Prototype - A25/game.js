2f67e0956a141e075b0302b49e278d98d9d6f8ac
Noah Darveau <nmdarveau@wpi.edu>
2021-04-11 18:35:41 -0400

    working on puzzle game

--- Puzzle Game Prototype - A25/game.js
+++ Puzzle Game Prototype - A25/game.js
@@ -65,6 +65,7 @@ let loadLevel = function(gridX, gridY, level) {
 	occupied = [];
 
 	let tempLevel = parsedLevels[level - 1];
+	PS.debug(tempLevel);
 
 	for (let i = 0; i < tempLevel.length; i++) {
 		let temp = tempLevel[i];
