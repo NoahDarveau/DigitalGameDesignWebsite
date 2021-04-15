8b2bbb8566243bd01da7fc8e3581de2399e798d3
Noah Darveau <nmdarveau@wpi.edu>
2021-04-15 02:11:20 -0400

    prototype v2

---
+++ Puzzle Game Prototype 2 - A28/game.js
@@ -0,0 +1,508 @@
+/*
+game.js for Perlenspiel 3.3.xd
+Last revision: 2021-04-08 (BM)
+
+Perlenspiel is a scheme by Professor Moriarty (bmoriarty@wpi.edu).
+This version of Perlenspiel (3.3.x) is hosted at <https://ps3.perlenspiel.net>
+Perlenspiel is Copyright © 2009-21 Brian Moriarty.
+This file is part of the standard Perlenspiel 3.3.x devkit distribution.
+
+Perlenspiel is free software: you can redistribute it and/or modify
+it under the terms of the GNU Lesser General Public License as published
+by the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+Perlenspiel is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU Lesser General Public License for more details.
+
+You may have received a copy of the GNU Lesser General Public License
+along with the Perlenspiel devkit. If not, see <http://www.gnu.org/licenses/>.
+*/
+
+/*
+This JavaScript file is a template for creating new Perlenspiel 3.3.x games.
+Add code to the event handlers required by your project.
+Any unused event-handling function templates can be safely deleted.
+Refer to the tutorials and documentation at <https://ps3.perlenspiel.net> for details.
+*/
+
+/*
+The following comment lines are for JSHint <https://jshint.com>, a tool for monitoring code quality.
+You may find them useful if your development environment is configured to support JSHint.
+If you don't use JSHint (or are using it with a configuration file), you can safely delete these lines.
+*/
+
+/* jshint browser : true, devel : true, esversion : 6, freeze : true */
+/* globals PS : true */
+
+"use strict"; // Do NOT delete this directive!
+
+//Global Variables
+let numLevels = 7;
+let currentLevel = 1;
+let levelImages = [];
+let levelSizes = [5, 6, 6, 7, 7, 8, 9];
+let parsedLevels = [];
+
+let tempPath = [];
+let setPaths = []
+let startSpots = [];
+let isDrawing;
+let currentColor;
+let numFinished = 0;
+let finishedCounts = [4, 6, 4, 7, 6, 7, 8, 10];
+let startLocation;
+
+let sounds = ["lineBreak", "enterBubble", "levelChange", "endHover"];
+
+
+let parseLevel = function(image) {
+	let tempLevel = [];
+	let data = image.data;
+
+	for (let i = 0; i < data.length; i++) {
+		let temp = {x: i % image.width, y: Math.floor(i / image.height), color: data[i]}
+		tempLevel.push(temp);
+	}
+
+	parsedLevels.push(tempLevel);
+
+	if (image.source === "Levels/level1.gif") {
+		loadLevel(levelSizes[0], levelSizes[0], 1);
+	}
+
+	//PS.debug("finished parsing " + image.source+'\n');
+};
+
+let loadLevel = function(gridX, gridY, level) {
+	startSpots = [];
+	tempPath = [];
+	setPaths = [];
+	//PS.debug("loading level: " + level);
+	PS.gridSize(gridX, gridY);
+	PS.radius(PS.ALL, PS.ALL, 50);
+	PS.border(PS.ALL, PS.ALL, 0);
+	PS.fade(PS.ALL, PS.ALL, 15);
+	PS.gridColor(0x30b8c9);
+	PS.borderColor(PS.ALL, PS.ALL, 0x000000);
+
+	PS.statusText("Level: " + currentLevel);
+
+	numFinished = 0;
+
+	let tempLevel = parsedLevels[level - 1];
+
+	for (let i = 0; i < tempLevel.length; i++) {
+		let temp = tempLevel[i];
+		PS.color(temp.x, temp.y, temp.color);
+		if (PS.color(temp.x, temp.y) !== PS.COLOR_WHITE) {
+			startSpots.push({x: temp.x, y: temp.y});
+		}
+	}
+
+	if (currentLevel < numLevels) {
+		levelImages.push(PS.imageLoad("Levels/level" + (currentLevel + 1) + ".gif", parseLevel, 1));
+	}
+
+	PS.audioPlay(sounds[2], {path: "Sounds/", fileTypes: ["mp3"]});
+
+};
+
+let breakPath = function() {
+	let temp = tempPath.length;
+	isDrawing = false;
+	for (let i = 0; i < tempPath.length; i++) {
+		PS.color(tempPath[i].x, tempPath[i].y, PS.COLOR_WHITE);
+	}
+
+	PS.border(PS.ALL, PS.ALL, 0);
+
+	tempPath = [];
+
+	if (temp > 0) {
+		PS.audioPlay(sounds[0], {path: "Sounds/", fileTypes: ["mp3"]});
+	}
+};
+
+let isStartSpot = function(x, y) {
+	for(let i = 0; i < startSpots.length; i++) {
+		if (x === startSpots[i].x && y === startSpots[i].y) {
+			return true;
+		}
+	}
+
+	return false;
+};
+
+let pushToSetPath = function() {
+	for (let i = 0; i < tempPath.length; i++) {
+		setPaths.push(tempPath[i]);
+	}
+	tempPath = [];
+};
+
+let clearColor = function(color) {
+	let flag = false;
+	for (let i = 0; i < setPaths.length; i++) {
+		if (PS.color(setPaths[i].x, setPaths[i].y) === color) {
+			PS.color(setPaths[i].x, setPaths[i].y, PS.COLOR_WHITE);
+			//PS.debug("Clearing color "+color+'\n');
+			flag = true;
+		}
+	}
+	let j = 0;
+	while (j > 0 && j < setPaths.length) {
+		if (PS.color(setPaths[j].x, setPaths[j].y) === PS.COLOR_WHITE) {
+			//PS.debug("here");
+			setPaths.splice(j, 1);
+			j--;
+		}
+		j++;
+	}
+
+	if (flag) {
+		numFinished--;
+		//PS.debug("Subtracting a finish\n");
+	}
+}
+
+let nextLevel = function() {
+	if (currentLevel < numLevels) {
+		currentLevel++;
+		loadLevel(levelSizes[currentLevel - 1], levelSizes[currentLevel - 1], currentLevel);
+	}
+}
+
+/*
+PS.init( system, options )
+Called once after engine is initialized but before event-polling begins.
+This function doesn't have to do anything, although initializing the grid dimensions with PS.gridSize() is recommended.
+If PS.grid() is not called, the default grid dimensions (8 x 8 beads) are applied.
+Any value returned is ignored.
+[system : Object] = A JavaScript object containing engine and host platform information properties; see API documentation for details.
+[options : Object] = A JavaScript object with optional data properties; see API documentation for details.
+*/
+
+PS.init = function( system, options ) {
+	// Uncomment the following code line
+	// to verify operation:
+
+	// PS.debug( "PS.init() called\n" );
+
+	// This function should normally begin
+	// with a call to PS.gridSize( x, y )
+	// where x and y are the desired initial
+	// dimensions of the grid.
+	// Call PS.gridSize() FIRST to avoid problems!
+	// The sample call below sets the grid to the
+	// default dimensions (8 x 8).
+	// Uncomment the following code line and change
+	// the x and y parameters as needed.
+
+	PS.gridSize( 5, 5 );
+
+	//PS.radius(PS.ALL, PS.ALL, 50);
+
+	//Set Borders
+	PS.border(PS.ALL, 0, {top: 10, left: 0, bottom: 0, right: 0});
+
+	// This is also a good place to display
+	// your game title or a welcome message
+	// in the status line above the grid.
+	// Uncomment the following code line and
+	// change the string parameter as needed.
+
+	PS.statusText( "Connect The Dots" );
+
+	// Add any other initialization code you need here.
+
+	//load levels
+	levelImages.push(PS.imageLoad("Levels/level1.gif", parseLevel, 1));
+
+	//load sounds
+	for (let i = 0; i < sounds.length; i++) {
+		PS.audioLoad(sounds[i], {path: "Sounds/", fileTypes: ["mp3"]});
+	}
+
+
+
+
+	// Change this TEAM constant to your team name,
+	// using ONLY alphabetic characters (a-z).
+	// No numbers, spaces, punctuation or special characters!
+
+	const TEAM = "spade";
+
+	// This code should be the last thing
+	// called by your PS.init() handler.
+	// DO NOT MODIFY IT, except for the change
+	// explained in the comment below.
+
+	PS.dbLogin( "imgd2900", TEAM, function ( id, user ) {
+		if ( user === PS.ERROR ) {
+			return;
+		}
+		PS.dbEvent( TEAM, "startup", user );
+		PS.dbSend( TEAM, PS.CURRENT, { discard : true } );
+	}, { active : false } );
+	
+	// Change the false in the final line above to true
+	// before deploying the code to your Web site.
+};
+
+/*
+PS.touch ( x, y, data, options )
+Called when the left mouse button is clicked over bead(x, y), or when bead(x, y) is touched.
+This function doesn't have to do anything. Any value returned is ignored.
+[x : Number] = zero-based x-position (column) of the bead on the grid.
+[y : Number] = zero-based y-position (row) of the bead on the grid.
+[data : *] = The JavaScript value previously associated with bead(x, y) using PS.data(); default = 0.
+[options : Object] = A JavaScript object with optional data properties; see API documentation for details.
+*/
+
+PS.touch = function( x, y, data, options ) {
+
+	// Uncomment the following code line
+	// to inspect x/y parameters:
+
+	// PS.debug( "PS.touch() @ " + x + ", " + y + "\n" );
+
+	// Add code here for mouse clicks/touches
+	// over a bead.
+
+	if (isStartSpot(x, y)) {
+		isDrawing = true;
+		currentColor = PS.color(x, y);
+		startLocation = {x: x, y: y};
+		clearColor(currentColor);
+	}
+
+	//PS.debug(PS.color(x, y)+'\n');
+
+	if (numFinished < finishedCounts[currentLevel-1]) {
+		PS.statusText("Level: " + currentLevel);
+	}
+
+
+};
+
+/*
+PS.release ( x, y, data, options )
+Called when the left mouse button is released, or when a touch is lifted, over bead(x, y).
+This function doesn't have to do anything. Any value returned is ignored.
+[x : Number] = zero-based x-position (column) of the bead on the grid.
+[y : Number] = zero-based y-position (row) of the bead on the grid.
+[data : *] = The JavaScript value previously associated with bead(x, y) using PS.data(); default = 0.
+[options : Object] = A JavaScript object with optional data properties; see API documentation for details.
+*/
+
+PS.release = function( x, y, data, options ) {
+	// Uncomment the following code line to inspect x/y parameters:
+
+	// PS.debug( "PS.release() @ " + x + ", " + y + "\n" );
+
+	//PS.scale(PS.ALL, PS.ALL, 100);
+
+	// Add code here for when the mouse button/touch is released over a bead.
+
+	//PS.debug("x: "+x+", y: "+y+'\n');
+
+	if (isStartSpot(x, y)) {
+		PS.border(PS.ALL, PS.ALL, 0);
+	}
+
+	if (isDrawing && isStartSpot(x, y) && PS.color(x, y) === currentColor && (x !== startLocation.x || y !== startLocation.y)) {
+		numFinished++;
+		pushToSetPath();
+		isDrawing = false;
+	}
+	else {
+		breakPath();
+	}
+
+	if (numFinished >= finishedCounts[currentLevel-1]) {
+		PS.statusText("YOU WIN, Press 'r' to restart");
+		nextLevel();
+	}
+
+	//PS.debug(numFinished);
+
+
+};
+
+/*
+PS.enter ( x, y, button, data, options )
+Called when the mouse cursor/touch enters bead(x, y).
+This function doesn't have to do anything. Any value returned is ignored.
+[x : Number] = zero-based x-position (column) of the bead on the grid.
+[y : Number] = zero-based y-position (row) of the bead on the grid.
+[data : *] = The JavaScript value previously associated with bead(x, y) using PS.data(); default = 0.
+[options : Object] = A JavaScript object with optional data properties; see API documentation for details.
+*/
+
+PS.enter = function( x, y, data, options ) {
+	// Uncomment the following code line to inspect x/y parameters:
+
+	// PS.debug( "PS.enter() @ " + x + ", " + y + "\n" );
+
+	if (isDrawing) {
+		PS.audioPlay(sounds[1], {path: "Sounds/", fileTypes: ["mp3"]});
+	}
+
+	if (isStartSpot(x, y)) {
+		PS.border(x, y, 3);
+		//PS.audioPlay(sounds[3], {path: "Sounds/", fileTypes: ["mp3"]});
+	}
+
+	// Add code here for when the mouse cursor/touch enters a bead.
+
+	if (isDrawing) {
+		if (!isStartSpot(x, y)) {
+			if (PS.color(x, y) === PS.COLOR_WHITE) {
+				tempPath.push({x: x, y: y});
+				PS.color(x, y, currentColor);
+			}
+			else {
+				breakPath();
+				//PS.debug("here");
+			}
+		}
+		else if (isStartSpot(x, y) && (PS.color(x, y) !== currentColor || (x === startLocation.x && y === startLocation.y))) {
+			breakPath();
+		}
+
+	}
+
+};
+
+/*
+PS.exit ( x, y, data, options )
+Called when the mouse cursor/touch exits bead(x, y).
+This function doesn't have to do anything. Any value returned is ignored.
+[x : Number] = zero-based x-position (column) of the bead on the grid.
+[y : Number] = zero-based y-position (row) of the bead on the grid.
+[data : *] = The JavaScript value previously associated with bead(x, y) using PS.data(); default = 0.
+[options : Object] = A JavaScript object with optional data properties; see API documentation for details.
+*/
+
+PS.exit = function( x, y, data, options ) {
+	// Uncomment the following code line to inspect x/y parameters:
+
+	// PS.debug( "PS.exit() @ " + x + ", " + y + "\n" );
+
+	// Add code here for when the mouse cursor/touch exits a bead.
+
+	if (!isDrawing && isStartSpot(x, y)) {
+		PS.border(x, y, 0);
+	}
+
+	if (isDrawing && isStartSpot(x, y) && (x !== startLocation.x || y !== startLocation.y)) {
+		breakPath();
+	}
+
+	//if (!isDrawing && isStartSpot(x, y)) {
+	//	PS.scale(x, y, 100);
+	//}
+};
+
+/*
+PS.exitGrid ( options )
+Called when the mouse cursor/touch exits the grid perimeter.
+This function doesn't have to do anything. Any value returned is ignored.
+[options : Object] = A JavaScript object with optional data properties; see API documentation for details.
+*/
+
+PS.exitGrid = function( options ) {
+	// Uncomment the following code line to verify operation:
+
+	//PS.debug( "PS.exitGrid() called\n" );
+	//PS.scale(PS.ALL, PS.ALL, 100);
+
+
+
+	// Add code here for when the mouse cursor/touch moves off the grid.
+	if (isDrawing) {
+		breakPath();
+	}
+};
+
+/*
+PS.keyDown ( key, shift, ctrl, options )
+Called when a key on the keyboard is pressed.
+This function doesn't have to do anything. Any value returned is ignored.
+[key : Number] = ASCII code of the released key, or one of the PS.KEY_* constants documented in the API.
+[shift : Boolean] = true if shift key is held down, else false.
+[ctrl : Boolean] = true if control key is held down, else false.
+[options : Object] = A JavaScript object with optional data properties; see API documentation for details.
+*/
+
+PS.keyDown = function( key, shift, ctrl, options ) {
+	// Uncomment the following code line to inspect first three parameters:
+
+	// PS.debug( "PS.keyDown(): key=" + key + ", shift=" + shift + ", ctrl=" + ctrl + "\n" );
+
+	// Add code here for when a key is pressed.
+
+	if (key === 82 || key === 114) {
+		currentLevel = 1;
+		loadLevel(levelSizes[currentLevel-1], levelSizes[currentLevel-1], currentLevel);
+	}
+};
+
+/*
+PS.keyUp ( key, shift, ctrl, options )
+Called when a key on the keyboard is released.
+This function doesn't have to do anything. Any value returned is ignored.
+[key : Number] = ASCII code of the released key, or one of the PS.KEY_* constants documented in the API.
+[shift : Boolean] = true if shift key is held down, else false.
+[ctrl : Boolean] = true if control key is held down, else false.
+[options : Object] = A JavaScript object with optional data properties; see API documentation for details.
+*/
+
+PS.keyUp = function( key, shift, ctrl, options ) {
+	// Uncomment the following code line to inspect first three parameters:
+
+	// PS.debug( "PS.keyUp(): key=" + key + ", shift=" + shift + ", ctrl=" + ctrl + "\n" );
+
+	// Add code here for when a key is released.
+};
+
+/*
+PS.input ( sensors, options )
+Called when a supported input device event (other than those above) is detected.
+This function doesn't have to do anything. Any value returned is ignored.
+[sensors : Object] = A JavaScript object with properties indicating sensor status; see API documentation for details.
+[options : Object] = A JavaScript object with optional data properties; see API documentation for details.
+NOTE: Currently, only mouse wheel events are reported, and only when the mouse cursor is positioned directly over the grid.
+*/
+
+PS.input = function( sensors, options ) {
+	// Uncomment the following code lines to inspect first parameter:
+
+	//	 var device = sensors.wheel; // check for scroll wheel
+	//
+	//	 if ( device ) {
+	//	   PS.debug( "PS.input(): " + device + "\n" );
+	//	 }
+
+	// Add code here for when an input event is detected.
+};
+
+/*
+PS.shutdown ( options )
+Called when the browser window running Perlenspiel is about to close.
+This function doesn't have to do anything. Any value returned is ignored.
+[options : Object] = A JavaScript object with optional data properties; see API documentation for details.
+NOTE: This event is generally needed only by applications utilizing networked telemetry.
+*/
+
+PS.shutdown = function( options ) {
+	// Uncomment the following code line to verify operation:
+
+	// PS.debug( "“Dave. My mind is going. I can feel it.”\n" );
+
+	// Add code here to tidy up when Perlenspiel is about to close.
+};
+
