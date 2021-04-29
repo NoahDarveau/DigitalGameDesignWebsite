9c72b910cbbc8e478e48a7ba941bb5ceebb6ce3a
Noah Darveau <nmdarveau@wpi.edu>
2021-04-29 13:52:46 -0400

    enabled logging

--- Restricted Game Prototype - A33/game.js
+++ Restricted Game Prototype - A33/game.js
@@ -307,7 +307,7 @@ PS.init = function( system, options ) {
 	// using ONLY alphabetic characters (a-z).
 	// No numbers, spaces, punctuation or special characters!
 
-	const TEAM = "teamname";
+	const TEAM = "spade";
 
 	// This code should be the last thing
 	// called by your PS.init() handler.
@@ -320,7 +320,7 @@ PS.init = function( system, options ) {
 		}
 		PS.dbEvent( TEAM, "startup", user );
 		PS.dbSend( TEAM, PS.CURRENT, { discard : true } );
-	}, { active : false } );
+	}, { active : true } );
 	
 	// Change the false in the final line above to true
 	// before deploying the code to your Web site.
